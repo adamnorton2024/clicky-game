29944197400107830514080c000a7f74eed4d7ed
Adam Norton <adam@adamnorton.com>
2019-11-13 17:41:20 -0600

    clicking and reshuffling together works, as well as score and keeping track of which images have been clicked login.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Container from './components/Container';
 import Jumbotron from './components/Jumbotron';
@@ -9,33 +9,128 @@ let shuffle = require('shuffle-array');
 
 let imgArray = [];
 
-for (let i = 0; i < images.length; i++) {
-  imgArray.push(images[i]);
-};
-
-shuffle(imgArray);
+class App extends Component {
 
 
+  //  Starting/Reset Values
+  state = {
+    currentScore: 0,
+    topScore: 0,
+    gameMessage: "",
+    clicked: [],
+    images,
+    gameOver: false
+  };
 
-function App() {
-  return (
-    <div>
-      <Jumbotron>
-        <Counter />
-      </Jumbotron>
-      <Container>
-      {imgArray.map(character =>
-        <GameCard 
-          image={character.image} 
-          name={character.name}
-          id={character.id}
-          clicked={character.clicked}
-           />
-      )}
-      </Container>
-    </div>
-  );
-  
+
+
+  // Render first set of images and starting game message
+  componentDidMount() {
+    for (let i = 0; i < images.length; i++) {
+      imgArray.push(images[i]);
+    };
+
+    shuffle(imgArray);
+
+    this.setState({gameMessage: "Click an image to start."})
+  };
+
+
+
+  // Shuffle and Redraw Images
+  shuffleImageArray = () => {
+    imgArray = [];
+
+    for (let i = 0; i < images.length; i++) {
+      imgArray.push(images[i]);
+    };
+
+    shuffle(imgArray);
+
+  }
+
+
+
+  // When image is clicked, increment score by 1, add image to array. 
+  clickedImage = (id) => {
+    console.log(`image ${id} clicked!`);
+    if(!this.state.clicked.includes(id)){
+      this.increaseScore();
+      this.state.clicked.push(id);
+      this.setState({
+        gameOver: false
+      });
+    } else {
+      this.resetGame();
+    }
+  };
+
+
+
+  // Increase Score by 1
+  increaseScore = () => {
+    let score = this.state.currentScore + 1;
+    if(score === this.state.images.length) {
+      this.setState({
+        topScore: score,
+        currentScore: 0,
+        gameMessage: "You win! Click an image to play again!",
+        clicked: [],
+        images,
+        gameOver: false
+      });
+    } else if(score > this.state.topScore) {
+      this.setState({
+        topScore:score,
+        currentScore: score,
+        gameMessage: "New High Score!"
+      });
+    } else {
+      this.setState({
+        currentScore: score,
+        gameMessage: "Correct!"
+      });
+    }
+    this.shuffleImageArray();
+  };
+
+
+  // Lose and Reset Game
+  resetGame = () => {
+    this.setState({
+      points: 0,
+      currentScore: 0,
+      topScore: this.state.topScore,
+      gameMessage: "Game Over! You Lose This Time.",
+      clicked: [],
+      images,
+      gameOver: true
+    });
+    // console.log("Game Over");
+    this.shuffleImageArray();
+  };
+
+
+  render(){
+    return (
+      <div>
+        <Jumbotron>
+          <Counter />
+        </Jumbotron>
+        <Container>
+          {imgArray.map(character =>
+            <GameCard
+              image={character.image}
+              name={character.name}
+              id={character.id}
+              clickedImage={this.clickedImage}
+            />
+          )}
+        </Container>
+      </div >
+    );
+  }
+
 };
 
 export default App;
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -4,9 +4,8 @@ function GameCard(props){
     return(
         
         <div className="col-xs-4 col-sm-3 col-lg-2 text-dark p-0" id={props.id}>
-            <button type="button" className="btn">
+            <button type="button" className="btn" onClick={() => props.clickedImage(props.id)}>
                 <img src={props.image} className="img-fluid rounded" alt={props.name} />
-                <p>Clicked = {props.clicked.toString()}</p>
             </button>
         </div>     
     );
