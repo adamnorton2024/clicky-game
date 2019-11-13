53e3c1187b444d057a706c29e68d46d73cd9de37
Adam Norton <adam@adamnorton.com>
2019-11-13 16:00:27 -0600

    have images randomizing, and made them clickable.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Jumbotron from './components/Jumbotron';
 import Counter from './components/Counter';
 import GameCard from './components/GameCard';
 import images from './images.json';
+let shuffle = require('shuffle-array');
+
+let imgArray = [];
+
+for (let i = 0; i < images.length; i++) {
+  imgArray.push(images[i]);
+};
+
+shuffle(imgArray);
+
 
 
 function App() {
@@ -14,7 +24,7 @@ function App() {
         <Counter />
       </Jumbotron>
       <Container>
-      {images.map(character =>
+      {imgArray.map(character =>
         <GameCard 
           image={character.image} 
           name={character.name}
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function GameCard(props){
     return(
-
-        <div className="card col-md-3 text-dark p-0" id={props.id}>
-            <img src={props.image} className="img-fluid rounded" alt={props.name} />
-            <p>Clicked = {props.clicked}</p>
-        </div>      
+        
+        <div className="col-xs-4 col-sm-3 col-lg-2 text-dark p-0" id={props.id}>
+            <button type="button" className="btn">
+                <img src={props.image} className="img-fluid rounded" alt={props.name} />
+                <p>Clicked = {props.clicked.toString()}</p>
+            </button>
+        </div>     
     );
 };
 
