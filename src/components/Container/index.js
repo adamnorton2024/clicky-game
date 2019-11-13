0448730cafd8b8dc64127f15c76c0af5a13fe4c0
Adam Norton <adam@adamnorton.com>
2019-11-13 08:22:41 -0600

    moved images to public folder. Styling to fit image cards into rows of 4.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Container(props){
-    return <div className="container d-flex flex-wrap justify-content-center">
+    return <div className="container col-sm-12 d-flex flex-wrap justify-content-center">
         {props.children}
     </div>;
 };
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -3,11 +3,8 @@ import React from 'react';
 function GameCard(props){
     return(
 
-        <div className="card col-md-3 text-dark m-2" id={props.id}>
+        <div className="card col-md-3 text-dark p-0" id={props.id}>
             <img src={props.image} className="card-img-top img-fluid" alt={props.name} />
-                <div className="card-body">
-                    <p className="card-text">{props.name}</p>
-                </div>
         </div>      
     );
 };
