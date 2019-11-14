582e47da284f1e21872b7aed5fdf089fee5c80fd
Adam Norton <adam@adamnorton.com>
2019-11-13 19:58:13 -0600

    added a little spacing between the image elements.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import Container from './components/Container';
 import Jumbotron from './components/Jumbotron';
-import Counter from './components/Counter';
 import GameCard from './components/GameCard';
 import images from './images.json';
 let shuffle = require('shuffle-array');
--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function GameCard(props){
     return(
         
-        <div className="col-xs-4 col-sm-3 col-lg-2 text-dark p-0" id={props.id}>
+        <div className="col-xs-4 col-sm-3 col-lg-2 text-dark p-0 mt-2" id={props.id}>
             <button type="button" className="btn" onClick={() => props.clickedImage(props.id)}>
                 <img src={props.image} className="img-fluid rounded" alt={props.name} />
             </button>
