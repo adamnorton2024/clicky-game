6b2f287b4ce245a6f67f73d6a1085f081d6b553b
Adam Norton <adam@adamnorton.com>
2019-11-13 20:12:47 -0600

    Updates

---
+++ precache-manifest.9394509677219a1b2724c7d47c8a38cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4e94b66b4ed7c256f85e3d5fdef471",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4c1cf3d7412286d8205c",
+    "url": "/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8c7a7c6a0fb8f320978",
+    "url": "/clicky-game/static/js/2.1c69731d.chunk.js"
+  },
+  {
+    "revision": "4c1cf3d7412286d8205c",
+    "url": "/clicky-game/static/js/main.cf5dd943.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  }
+]);
